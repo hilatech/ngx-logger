3f41dd61797b2e18aa4eb34390eb27880a001e44
Mohammad <moattarwork@msn.com>
2017-12-10 22:25:54 +0000

    #3 Initial packaing for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LogModule } from 'app/logging';
+import { LogModule } from './logging';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
--- src/app/logging/log.module.ts
+++ src/app/logging/log.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { LogObserver, LOG_LISTENER } from './log-observer.service';
-import { ConsoleListenerConfig, ConsoleListener } from 'app/logging/listeners/console-listener';
-import { ServerListenerConfig, ServerListener } from 'app/logging/listeners/server-listener';
+import { ConsoleListenerConfig, ConsoleListener } from './listeners/console-listener';
+import { ServerListenerConfig, ServerListener } from './listeners/server-listener';
 import { HttpClient } from '@angular/common/http';
-import { LogService } from 'app/logging/log.service';
+import { LogService } from './log.service';
 import { HttpClientModule } from '@angular/common/http';
-import { LogServiceFactory } from 'app/logging';
+import { LogServiceFactory } from './log-service-factory';
 
 @NgModule({
   imports: [
@@ -33,5 +33,6 @@ export class LogModule {
     };
   }
 }
+
 
 
